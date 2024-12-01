24443928b3aaa536dc8a39ee2f19fc3419f5effb
James Diotte <JamesDiotte@gmail.com>
2024-12-01 17:18:27 -0500

    Day 1, part 2

--- solutions/day01.py
+++ solutions/day01.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def load_id_lists(docuemnt_path: str) -> list[str]:
     return open(docuemnt_path).read().split("\n")
 
@@ -16,13 +19,24 @@ def get_total_distance(list1: list[int], list2: list[int]) -> int:
     return sum(abs(x - y) for x, y in zip(list1, list2))
 
 
+def get_total_similarity(list_l: list[int], list_r: list[int]) -> int:
+    multiplicity_dict: dict[int, int] = defaultdict(int)
+    set_l = set(list_l)
+    for id_r in list_r:
+        if id_r in set_l:
+            multiplicity_dict[id_r] += id_r
+    return sum(multiplicity_dict[id_l] for id_l in list_l)
+
+
 # Test
 test_list_raw = load_id_lists("inputs/day01/test.txt")
 test_list_l, test_list_r = get_ordered_id_lists(test_list_raw)
 assert get_total_distance(test_list_l, test_list_r) == 11
+assert get_total_similarity(test_list_l, test_list_r) == 31
 
 
 # Main Solution
 list_raw = load_id_lists("inputs/day01/main.txt")
 list_l, list_r = get_ordered_id_lists(list_raw)
 print(f"Part 1: {get_total_distance(list_l, list_r)}")
+print(f"Part 2: {get_total_similarity(list_l, list_r)}")
