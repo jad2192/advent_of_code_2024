b0d87e0ae69b6e2a0d30a6031c3cb7545e258499
James Diotte <james@findanomaly.com>
2024-12-09 17:08:57 -0500

    Day 9 speedup

--- solutions/day09.py
+++ solutions/day09.py
@@ -2,8 +2,40 @@ def load_disk_map(document_path: str) -> str:
     return open(document_path).read().strip()
 
 
-def parse_disk(disk_map: str) -> tuple[list[int], list[list[int]], list[str]]:
-    free_space_locs, free_space_blocks, file = [], [], []
+class FreeSpaceQueue:
+    def __init__(self):
+        self.stack_dict = {k: [] for k in range(1, 10)}
+        self.stack_priority = list(range(1, 10))
+
+    def update_priority(self):
+        self.stack_priority = sorted(
+            [k for k in self.stack_dict if len(self.stack_dict[k]) > 0], key=lambda k: self.stack_dict[k][0][0]
+        )
+
+    def push(self, index_block: list[int], sort=False):
+        if len(index_block) > 0:
+            self.stack_dict[len(index_block)].append(index_block)
+            if sort:
+                self.stack_dict[len(index_block)].sort(key=lambda b: b[0])
+            self.update_priority()
+
+    def pop(self, file_index_block: list[int]) -> list[int] | None:
+        index_block = None
+        for k in self.stack_priority:
+            if k >= len(file_index_block) and self.stack_dict[k][0][0] < file_index_block[0]:
+                index_block = self.stack_dict[k].pop(0)
+                break
+        if index_block is not None:
+            if len(index_block) > len(file_index_block):
+                new_block = index_block[len(file_index_block) :]
+                self.push(new_block, sort=True)
+            else:
+                self.update_priority()
+        return index_block
+
+
+def parse_disk(disk_map: str) -> tuple[list[int], FreeSpaceQueue, list[str]]:
+    free_space_locs, free_space_queue, file = [], FreeSpaceQueue(), []  # type:ignore
     for k, char in enumerate(disk_map):
         start_ix = len(file)
         if k % 2 == 0:
@@ -11,8 +43,8 @@ def parse_disk(disk_map: str) -> tuple[list[int], list[list[int]], list[str]]:
         else:
             file.extend(["."] * int(char))
             free_space_locs.extend(range(start_ix, len(file)))
-            free_space_blocks.append(list(range(start_ix, len(file))))
-    return free_space_locs, free_space_blocks, file
+            free_space_queue.push(list(range(start_ix, len(file))))
+    return free_space_locs, free_space_queue, file
 
 
 def rearrange_file(free_space_locs: list[int], file: list[str]) -> list[str]:
@@ -24,7 +56,7 @@ def rearrange_file(free_space_locs: list[int], file: list[str]) -> list[str]:
     return file
 
 
-def rearrange_file_by_blocks(free_space_blocks: list[list[int]], file: list[str]) -> list[str]:
+def rearrange_file_by_blocks(free_space_queue: FreeSpaceQueue, file: list[str]) -> list[str]:
     file_blocks, index_blocks = [], []
     cur_block, curr_ix_block = [file[0]], [0]
     for k in range(1, len(file)):
@@ -40,14 +72,11 @@ def rearrange_file_by_blocks(free_space_blocks: list[list[int]], file: list[str]
         index_blocks.append(curr_ix_block)
     for file_bk, ix_bk in zip(file_blocks[::-1], index_blocks[::-1]):
         if file_bk[0] != ".":
-            for k in range(len(free_space_blocks)):
-                free_bk = free_space_blocks[k]
-                if free_bk and (free_bk[0] < ix_bk[0]) and (len(ix_bk) <= len(free_bk)):
-                    # Track block start / end indices
-                    ix1, ix2, ix3, ix4 = free_bk[0], free_bk[0] + len(ix_bk), ix_bk[0], ix_bk[-1] + 1
-                    file[ix1:ix2], file[ix3:ix4] = file[ix3:ix4], file[ix1:ix2]  # Swap locs in file
-                    free_space_blocks[k] = free_space_blocks[k][len(ix_bk) :]  # Update free space block
-                    break
+            free_bk = free_space_queue.pop(ix_bk)
+            if free_bk:
+                # Track block start / end indices
+                ix1, ix2, ix3, ix4 = free_bk[0], free_bk[0] + len(ix_bk), ix_bk[0], ix_bk[-1] + 1
+                file[ix1:ix2], file[ix3:ix4] = file[ix3:ix4], file[ix1:ix2]  # Swap locs in file
     return file
 
 
